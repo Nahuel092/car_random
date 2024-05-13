9af2e6720ad27834ab97d543a1c8da2845b7bab9
Argento <Naahuel92@hotmail.com>
2024-05-13 16:48:11 -0300

    se agrego el config

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+Config.lista = {
+    'blista',
+    'prairie',
+    'previon',
+    'dukes',
+    'everon',
+    'trophytruck',
+    'tailgater2',
+    'warrener',
+    'drafter',
+    'schafter2',
+    'sugoi',
+    'xa21'
+}
